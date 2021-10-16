6e156730f5fe77cb82faa9da36b4452cc22c7baf
Nimendra Kariyawasam <57085116+nimendrak@users.noreply.github.com>
2021-10-16 09:39:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,28 +10,34 @@ print(pg.position())
 
 pg.click(385, 1010)
 
-arr = ["hi guys!", "how are you guys?", "uncovered gonna be a hit", "wassup?", "uncovered FTW", "love from Asia",
-"GG!", "I wanna get into the WL so bad", "LFG", "support uncovered", "yo!", "haha", "lets go to the moon", "this is the NFT era",
-"we are going to the moooon!", "I want WL", "WLLLL!", "WLL!", "WL!", "hello!", "lets make this project a hit", "love this art works tho",
-"follow, follow, follow", "this is gonna be a lit community ppl", "uncovered !!!", "NFT foreverrr", "WOW", "I love this project", "WAGMI",
-"boom!", "fantastic ppl", "let's grind baby", "GRINDDD", "let's grind ppl", "haha", "niceee", "want this", "I want this sooo bad", "mint is coming",
-"need this so bad", "dont stop ppl", "we can do this", "keep going", "up up up", "looks dope tho", "LVL10", "lessgooo", "we may have a chance to get into the WL",
-"we can do this", "moooooon", "mooooon", "moon", "I want LVL10", "grind so haaard!", "we can do this", "server gone mad!", "you guys rock!",
-"gooooooooo", "to the moooooooon", "server is live af", "so many ppl active rm", "damn", "love this server"]
+arr = ["uncovered gonna be a hit", "uncovered FTW",
+"GG!", "LFG", "support uncovered", "yo!", "haha", "lets go to the moon",
+"we are going to the moooon!", "lets make this project a hit", "love this art works tho",
+"this is gonna be a lit community ppl", "uncovered !!!", "NFT foreverrr", "WOW", "I love this project", "WAGMI",
+"boom!", "fantastic ppl", "let's grind baby", "GRINDDD", "let's grind ppl", "haha", "niceee", "want this", "I want this sooo bad", "mint is here",
+"need this so bad", "dont stop ppl", "we can do this", "keep going", "up up up", "looks dope tho", "LVL10", "lessgooo",
+"we can do this", "moooooon", "mooooon", "moon", "GL reaching LVL10", "keep grinding ppl!", "we can do this", "server is mad rn!", "you guys rock!",
+"gooooooooo", "to the moooooooon", "server is live af", "love this server", "the hype is real", "uncocvred project is awesome",
+"fasted growing community ever", "lets go fam!", "really hope I to get in touch with 1/1", "love these sneak peak", "this is an awesome project", "just keep grinding"]
 
 localtime = time.localtime()
 random.seed(0)
 
 limit = 1000
 
+def getRandomTime():
+    timeArr = [30, 60]
+    return random.choice(timeArr)
+
 for i in range(0, limit):
-    result = time.strftime("%I:%M:%S %p", localtime)
+    timeStamp = time.strftime("%I:%M:%S %p", localtime)
     randomWrd = random.choice(arr)
+    randomTime = getRandomTime()
     # randomGen = gen.sentence()
     pg.typewrite(randomWrd, 0.1)
     pg.press("enter")
 
-    print(f"Index {limit - i} -> {result} - {randomWrd}")
-    time.sleep(3)
+    print(f"{limit - i} - {randomTime}s - {timeStamp} - {randomWrd}")
+    time.sleep(randomTime)
 
 print("\nALL DONE!")
