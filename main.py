0bbe0225b378535cc41ed3183cba15fd66b19051
NimendraK <nimendra.2019264@iit.ac.lk>
2021-10-16 11:43:51 +0530

    timestamp changed

--- main.py
+++ main.py
@@ -20,32 +20,30 @@ pg.click(385, 1010)
 # "gooooooooo", "to the moooooooon", "server is live af", "love this server", "the hype is real", "uncocvred project is awesome",
 # "fasted growing community ever", "lets go fam!", "really hope I to get in touch with 1/1", "love these sneak peak", "this is an awesome project", "just keep grinding"]
 
-arr = ["this is gonna be a hit",
-"LFG", "lets make this project a hit", "love this art works tho", "I love this project", "WAGMI", "let's grind baby", "GRINDDD", "let's grind ppl", "haha", "niceee", 
-"dont stop ppl", "we can do this", "keep going", "yeah, it looks dope tho", "LVL20 here I come", "lessgooo",
-"keep grinding ppl!", "we can do this", "server is mad rn!", "you guys rock!",
-"gooooooooo", "server is live af", "love this server", "the hype is real", "uncocvred project is awesome",
- "lets go fam!", "love these sneak peak", "this is an awesome project", "just keep grinding"]
+arr = ["this is gonna be a hit", "LFG", "lets make this project a hit", "love this art works tho", "I love this project", "WAGMI", "let's grind baby", "GRINDDD", "let's grind ppl", "I want this sooo bad", "niceee", 
+"dont stop ppl", "we can do this", "keep going", "yeah, it looks dope tho", "LVL20 here I come", "lessgooo", "keep grinding ppl!", "we can do this", "server is mad rn!", "you guys rock!", "we can hit the moon",
+"mooooon", "gooooooooo ppl we can do this", "server is live af", "love this server", "the hype is real", "uncocvred project is awesome", "lets go fam!", "love these sneak peak", "this is an awesome project",
+"just keep grinding", "this is gonna be a hella community", "imagine if you can hold a NFT helment, damn!", "that's nice tho", "i really Like that idea", "keep going, you can do this my friend", "hey newbie, wassup?"]
 
-
-localtime = time.localtime()
 random.seed(0)
-
 limit = 1000
 
 def getRandomTime():
-    timeArr = [30, 45, 60]
+    timeArr = [5, 6, 7]
     return random.choice(timeArr)
 
 for i in range(0, limit):
-    timeStamp = time.strftime("%I:%M:%S %p", localtime)
+    localtime = time.localtime()
+
+    timeStamp = time.strftime('%Y-%m-%d %H:%M:%S', localtime)
     randomWrd = random.choice(arr)
     randomTime = getRandomTime()
+
     # randomGen = gen.sentence()
     pg.typewrite(randomWrd, 0.1)
     pg.press("enter")
 
-    print(f"{limit - i} - {randomTime}s - {timeStamp} - {randomWrd}")
+    print(f"{str(i).zfill(3)} - {randomTime}s - {timeStamp} - {randomWrd}")
     time.sleep(randomTime)
 
 print("\nALL DONE!")
