9b3db5e43690d15a02e6447395ddccc345396fc5
NimendraK <nimendra.2019264@iit.ac.lk>
2021-10-16 12:55:39 +0530

    shuffle before pick

--- main.py
+++ main.py
@@ -15,18 +15,21 @@ with open('random_phrase.json') as json_file:
     data = json.load(json_file)
 
 def getRandomTime():
-    timeArr = [5, 6, 7]
+    # timeArr = [30, 45, 60]
+    timeArr = [3, 4, 5]
+    random.shuffle(timeArr)
     return random.choice(timeArr)
 
 for i in range(0, limit):
     localtime = time.localtime()
-
     timeStamp = time.strftime('%Y-%m-%d %H:%M:%S', localtime)
+
+    random.shuffle(data)
     randomWrd = random.choice(data)
     randomTime = getRandomTime()
 
-    pg.typewrite(randomWrd, 0.1)
-    pg.press("enter")
+    # pg.typewrite(randomWrd, 0.1)
+    # pg.press("enter")
 
     print(f"{str(i).zfill(3)} - {randomTime}s - {timeStamp} - {randomWrd}")
     time.sleep(randomTime)
